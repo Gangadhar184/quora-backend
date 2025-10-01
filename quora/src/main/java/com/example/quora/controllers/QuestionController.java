acd03524d1df8e487a0c11a06c520abac6b68dc2
Gangadhar184 <zenistuig65@gmail.com>
2025-10-01 18:40:37 +0530

    implemented QuestionController

---
+++ quora/src/main/java/com/example/quora/controllers/QuestionController.java
@@ -0,0 +1,36 @@
+package com.example.quora.controllers;
+
+
+import com.example.quora.models.Question;
+import com.example.quora.services.QuestionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+public class QuestionController {
+    private QuestionService questionService;
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public List<Question> getAllQuestions(@RequestParam int offset, @RequestParam int limit) {
+        return questionService.getQuestions(offset, limit);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Question> getQuestionById(@PathVariable Long id){
+        Optional<Question> question = questionService.getQuestionById(id);
+        return question.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteQuestion(@PathVariable Long id){
+        questionService.deleteQuestion(id);
+        return ResponseEntity.noContent().build();
+    }
+}
