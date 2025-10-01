581658edc192a83ce919792cd64fcbe5ede34b7f
Gangadhar184 <zenistuig65@gmail.com>
2025-10-01 13:08:45 +0530

    added pagination in repository

--- quora/src/main/java/com/example/quora/repositories/CommentsRepository.java
+++ quora/src/main/java/com/example/quora/repositories/CommentsRepository.java
@@ -1,7 +1,13 @@
 package com.example.quora.repositories;
 
 import com.example.quora.models.Comments;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentsRepository extends JpaRepository< Comments, Long> {
+    Page<Comments> findByAnswerId(Long answerId, Pageable pageable);
+    Page<Comments> findParentCommentId(Long parentCommentId, Pageable pageable);
 }
